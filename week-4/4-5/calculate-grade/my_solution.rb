ebf94143bbc6857da20d7a4d4a28154c4d901426
JonClayton <jonclayton@swbell.net>
2015-10-22 15:47:46 -0500

    Solve problems elegantly in one line

--- week-4/4-5/calculate-grade/my_solution.rb
+++ week-4/4-5/calculate-grade/my_solution.rb
@@ -1,6 +1,51 @@
 # Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Gregg Wehmeier.
 
+# Jon's three approaches:
+# 1. case/when
+# 2. if then elsif elsif
+# 3. formula to get 0,1,2, and then pull from grade array ['A','B','C']
 
-# Your Solution Below
+# Various methods for solution
+def various_get_grade (grade)
+  method=4
+  case method
+  when 1
+    if grade < 60
+      return 'F'
+      elsif grade < 70 
+        return 'D'
+        elsif grade < 80
+        return 'C'
+          elsif grade < 90
+          return 'B'
+          else
+            return 'A'
+    end
+  when 2
+    case
+    when grade<60
+      return 'F'
+    when grade<70
+      return 'D'
+    when grade<80
+      return 'C'
+    when grade<90
+      return 'B'
+    else 
+      return 'A'
+    end
+  when 3
+    grades = ["A","B","C","D","F","A"]
+    index = [(99-grade)/10,4].min
+    return grades[index]
+  when 4
+    return ["A","B","C","D","F","A"][[(99-grade)/10,4].min]
+  end
+end
+
+# A single expression solution
+def get_grade(grade)
+  ["A","B","C","D","F","A"][[(99-grade)/10,4].min]
+end
--- week-4/4-5/leap-years/my_solution.rb
+++ week-4/4-5/leap-years/my_solution.rb
@@ -1,6 +1,41 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Gregg Wehmeier.
 
+# Don't forget the 100 and 400 flavor to leap years
 
 # Your Solution Below
+
+#   Jon's solution with four options
+def options_leap_year?(year)
+	method = 4
+	case method
+	when 1
+		case
+		when year % 400 == 0
+		return true
+		when year % 100 == 0
+	    return false
+		when year % 4 > 0
+	    return false
+    end
+    when 2
+        return false if year % 4 > 0 || (year % 100 == 0 && year % 400 >0)
+    when 3
+    	if year % 4 > 0
+    		return false
+    	elsif year % 400 == 0
+    		return true
+    	elsif year % 100 == 0 
+    		return false
+    	end 
+    when 4
+        return !(year % 4 > 0 || (year % 100 == 0 && year % 400 >0))
+    end
+    true
+end
+
+# A compact single expression solution
+def leap_year?(year)
+    !(year%4>0||(year%100==0&&year%400>0))
+end
