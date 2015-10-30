8cc6d72aca87ba3771a6113513ebd7b44fb52bbb
JonClayton <jonclayton@swbell.net>
2015-10-30 03:02:48 -0500

    Hold changes to 5-3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -147,11 +147,14 @@ def mode(array)
     array.uniq.each {|element| array.delete_at(array.index(element))} 
   end
   array.sort
-    
+   
   when "recursive"
-    return array.sort if array == array.uniq
-    array.uniq.each {|element| array.delete_at(array.index(element))}
-    mode(array)
+  require 'set'
+  s1 = Set.new array
+  puts s1
+#    return array.sort if array == array.uniq
+ #   array.uniq.each {|element| array.delete_at(array.index(element))}
+  #  mode(array)
     
   when "and finally" # we get down to one line, though I wish the block inside #each was more elegant
     array == array.uniq ? array.sort : array.uniq.each {|element| array.delete_at(array.index(element))} && mode(array)
