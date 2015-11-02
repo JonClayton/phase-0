2a80abe22d63ba938c6c5329145495e9eb728308
JonClayton <jonclayton@swbell.net>
2015-11-02 15:54:33 -0600

    Add 6.4 solution

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,18 +1,30 @@
 #Attr Methods
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge by myself.
 
-# I spent [#] hours on this challenge.
+# I spent 1 hours on this challenge.
 
 class NameData
-
+	attr_reader :name
+	def initialize
+		@name = "Jon"
+	end
 end
 
 
 class Greetings
+	def initialize
+		@student = NameData.new
+	end
+
+	def hello
+		puts "Hello Student! How wonderful to see you today"
+		puts "But I shouldn't call you Student when I know your name is #{@student.name}"
+	end
 
 end
 
-
+greet = Greetings.new
+greet.hello
 
 # Reflection
