f87984a7d9fee19f1326a8af96b2a8f955ca935f
JonClayton <jonclayton@swbell.net>
2015-11-04 12:27:24 -0600

    JK this is the one with the change to 6.2

--- week-6/die/my_solution.rb
+++ week-6/die/my_solution.rb
@@ -36,12 +36,13 @@ end
 =end
 # Refactored Solution
 # I really don't see a refactoring opportunity here other than writing more descriptive 
-# variable names so I will do that and simplify the random number
+# variable names so I will do that and simplify the random number, along with correcting it 
+# because I misunderstood how rand(num) works!
 
 class Die
   def initialize(sides)
     raise ArgumentError.new("Input of sides must be an array") unless sides.is_a?(Array)
- 	@side_list = sides
+ 	  @side_list = sides
     @side_count = sides.size
     raise ArgumentError.new("Number of sides can't be less than 1") unless @side_count> 0
   end
@@ -51,15 +52,11 @@ class Die
   end
 
   def roll
-    @side_list[rand(@side_count)-1]
+    @side_list[rand(@side_count)]
   end
 end
 
-
-
-
 =begin
-# Reflection
 ##What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
 There wasn't much difference. I just needed to add a little bit of logic to calculate the number of sides, check that the input was an array, and convert to roll to a side label instead of a side number.
 
@@ -67,7 +64,7 @@ There wasn't much difference. I just needed to add a little bit of logic to calc
 KISS--keep it simple (we'll leave out the end of the acronym)
 
 ##What new methods did you learn when working on this challenge, if any?
-I finally realized I could just use rand(an_integer) instead of Random.rand(a_range).
+I finally realized I could just use `rand(an_integer)` and adjust down to get an index number instead of `Random.rand(a_range)`.  Then later in the week I realized I was confused about the output of `#rand`.  How embarassing...had to come back here and take out a minus -1 that I thought was necessary but wasn't.  But in fact it didn't matter since an index range of -1 to 4 will get you all six sides as results, even though it's unconventional.  You could retrieve the side with -6 though -1 if you wanted to!
 
 ##What concepts about classes were you able to solidify in this challenge?
 I think we are seeing them get more flexible.
