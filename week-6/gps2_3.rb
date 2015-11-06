7cfe2bf311c9585c5d2a01b2b4ee244ac56095a8
Jon Clayton <Jonclayton@swbell.net>
2015-11-05 22:32:52 -0600

    Update gps2_3.rb

--- week-6/gps2_3.rb
+++ week-6/gps2_3.rb
@@ -1,34 +1,85 @@
 # Your Names
-# 1)
-# 2)
+# 1) Jon Clayton
+# 2) Van Phan 
 
 # We spent [#] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
+# def serving_size_calc(item_to_make, order_quantity)
+#   # this number of servings provided per recipe
+#   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+#   # need to replace this method of comparing input to library keys and generating error message if needed through line 23
+  
+#   error_counter = 3
+
+#   library.each do |food|
+#     if library[food] != library[item_to_make]
+#       p error_counter += -1
+#     end
+#   end
+
+#   if error_counter > 0
+#     raise ArgumentError.new("#{item_to_make} is not a valid input")
+#   end
+#   # how many servings from one batch---need to clean up code
+#   # Calculate how many full batches are needed to meet order requirement
+#   # calculate how many leftover servings will be generated
+#   # report above in sentence format
+#   # example: p serving_size_calc("pie", 15) #=> "Make 3 pies.  You will have 6 servings left over, which is equivalent to 0 pies, 1 cake, and 1 cookie."
+  
+#   serving_size = library.values_at(item_to_make)[0]
+#   serving_size_mod = order_quantity % serving_size
+
+#   case serving_size_mod
+#   when 0
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+#   else
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+#   end
+# end
+
+#Refactored version
+
+def serving_size_calc(item_to_make, servings_needed)
+
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
+  raise ArgumentError.new("#{item_to_make} is not a valid input") unless library.has_key?(item_to_make)
+  
+  #How many of the item ordered are required?
+  servings_per_batch = library[item_to_make]
+  full_batches_needed = servings_needed / servings_per_batch
+  
+  #Did we meet servings_needed exactly or do we need more food?
+  extra_servings_required = servings_needed % servings_per_batch
+  extra_batch_needed = extra_servings_required > 0
+  leftover_servings = servings_per_batch - extra_servings_required
+  
+  #How to cover shortage if we don't make an extra of the desired item
+  cake_possible = extra_servings_required >= library["cake"]
+  cookies_offered = extra_servings_required % library["cake"]
+  
+  #generate clean text for main order
+  result = "Make #{full_batches_needed} #{item_to_make}#{full_batches_needed > 1 ? "s" : ""} to serve #{servings_needed} people."
+  
+  #generate text needed if the main order doesn't cover servings_needed exactly
+  if extra_batch_needed
+    extra_result = " This will leave you short #{extra_servings_required} servings.  You can either prepare an additional #{item_to_make}, with #{leftover_servings} servings left over, or you could make"
+    cake_result = "#{cake_possible ? " a cake" : ""}"
+    case cookies_offered
+      when 0
+      cookie_result = ""
+      when 1
+      cookie_result = " 1 cookie"
+    else 
+      cookie_result = " #{cookies_offered} cookies"
     end
+    connector = cake_result != "" && cookie_result != "" ? " and" : ""
+    closing_result = " so those people will have something to eat even if it is not the preferred item."
+    result = result + extra_result + cake_result + connector + cookie_result + closing_result
   end
-
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
-
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  end
+  return result
 end
 
 p serving_size_calc("pie", 7)
@@ -40,5 +91,3 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
-
-
