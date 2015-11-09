f08f66367f1854d18202822fca63e80a67339b54
JonClayton <jonclayton@swbell.net>
2015-11-09 15:38:48 -0600

    Add GPS solution

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -1,87 +1,67 @@
-# Virus Predictor
-
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
-
 # EXPLANATION OF require_relative
-#
-#
+# links to a file in the relative (same) location. Derivation of "require" which may be used to access direct links.
+
 require_relative 'state_data'
 
 class VirusPredictor
 
-  def initialize(state_of_origin, population_density, population)
-    @state = state_of_origin
-    @population = population
-    @population_density = population_density
+  # Loads the class with information
+  def initialize
   end
 
+  # Predicts the effects of the virus spread by calling two other class methods.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths 
+    speed_of_spread 
   end
-
+  
+  def all_states
+    STATE_DATA.each_key {|state| a_state(state)}
+  end
+  
+  def a_state(state)
+    @state = state
+    @population = STATE_DATA[state][:population]
+    @population_density = STATE_DATA[state][:population_density]      
+    virus_effects
+  end
+  
   private
 
-  def predicted_deaths(population_density, population, state)
+  # Predicts number of deaths based on a state's population and population density.
+  def predicted_deaths 
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    case @population_density
+      when (0...50)
+      @death_factor = 0.05
+      when (50...100)
+      @death_factor = 0.1
+      when (100...150)
+      @death_factor = 0.2      
+      when (150...200)
+      @death_factor = 0.3
     else
-      number_of_deaths = (@population * 0.05).floor
+      @death_factor = 0.4
     end
-
+    number_of_deaths = (@population * @death_factor).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  # Predicts the speed of virus spread within a state.
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
+    speed = 0.5 + 0.5 * [(250-@population_density.to_i)/50,0].max
+    puts " and the virus will spread across the state in #{speed} months.\n\n"
   end
-
+  
 end
-
 #=======================================================================
-
 # DRIVER CODE
- # initialize VirusPredictor for each state
-
-
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-
-
+# initialize VirusPredictor
+report = VirusPredictor.new
+report.all_states
+report.a_state("Illinois")
 #=======================================================================
 # Reflection Section
